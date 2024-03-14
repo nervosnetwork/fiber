170e53b306ccf6dda5b9a3f2ab1693a6762420f2
YI <uuuuuu@protonmail.com>
2024-03-15 00:41:19 +0800

    Add more comments about config

--- src/config.rs
+++ src/config.rs
@@ -11,18 +11,18 @@ use serde::{Deserialize, Serialize};
 
 use crate::{CkbConfig, LdkConfig};
 
-fn get_base_dir() -> PathBuf {
+const DEFAULT_CONFIG_FILE_NAME: &str = "config.yml";
+const DEFAULT_CKB_DIR_NAME: &str = "ckb";
+const DEFAULT_LDK_DIR_NAME: &str = "ldk";
+
+fn get_default_base_dir() -> PathBuf {
     let mut path = home_dir().expect("get home directory");
     path.push(".ckb-pcn-node");
     path
 }
 
-const DEFAULT_CONFIG_FILE_NAME: &str = "config.yml";
-const DEFAULT_CKB_DIR_NAME: &str = "ckb";
-const DEFAULT_LDK_DIR_NAME: &str = "ldk";
-
 fn get_default_config_file() -> PathBuf {
-    let mut path = get_base_dir();
+    let mut path = get_default_base_dir();
     path.push(DEFAULT_CONFIG_FILE_NAME);
     path
 }
@@ -54,10 +54,10 @@ struct Args {
     // under base dir.
     /// config file
     #[arg(short, long = "config", help = format!("config file [default: {:?} or $BASE_DIR/config.yml]", get_default_config_file()))]
-    config_path: Option<std::path::PathBuf>,
+    config_file: Option<std::path::PathBuf>,
 
     /// base directory
-    #[arg(short = 'd', long = "dir", help = format!("base directory for all [default: {:?}]", get_base_dir()))]
+    #[arg(short = 'd', long = "dir", help = format!("base directory for all [default: {:?}]", get_default_base_dir()))]
     base_dir: Option<std::path::PathBuf>,
 
     /// services to run (can be any of `ckb`/`ldk`, separated by `,`)
@@ -82,7 +82,9 @@ struct SerializedConfig {
 
 #[derive(Debug)]
 pub struct Config {
+    // ckb config, None represents that we should not run ckb service
     pub ckb: Option<CkbConfig>,
+    // ldk config, None represents that we should not run ldk service
     pub ldk: Option<LdkConfig>,
 }
 
@@ -100,10 +102,15 @@ impl Config {
         // Parse whole args with clap
         let mut args = Args::parse();
 
-        let base_dir = args.base_dir.clone().unwrap_or(get_base_dir());
+        // Base directory for all things to be stored to disk
+        let base_dir = args.base_dir.clone().unwrap_or(get_default_base_dir());
 
+        // Get config file by
+        // 1. Using the explictly set command line argument `config`
+        // 2. Prepending `config.yml` to the explictly set command line argument `dir`
+        // 3. Using the default `config.yml` file
         let config_file = args
-            .config_path
+            .config_file
             .or(args.base_dir.map(|x| x.join(DEFAULT_CONFIG_FILE_NAME)))
             .unwrap_or(get_default_config_file());
 
@@ -111,6 +118,11 @@ impl Config {
             serde_yaml::from_reader::<_, SerializedConfig>(f).expect("valid config file format")
         });
 
+        // Services to run can be passed from
+        // 1. command line
+        // 2. config file
+        // If command line arguments contain services, then don't read config file
+        // for services to run any more, otherwise use config file for that.
         let services = if args.services.is_empty() {
             config_from_file
                 .as_ref()
@@ -121,10 +133,11 @@ impl Config {
             args.services
         };
         if services.is_empty() {
-            error!("Must run at least one service");
+            error!("Must run at least one service. Specifying services to run by command line or config file.");
             print_help_and_exit(1)
         };
 
+        // Set default ckb/ldk base directory. These may be overridden by values explictly set by the user.
         args.ckb.base_dir = Some(Some(base_dir.join(DEFAULT_CKB_DIR_NAME)));
         args.ldk.base_dir = Some(Some(base_dir.join(DEFAULT_LDK_DIR_NAME)));
 
@@ -135,6 +148,7 @@ impl Config {
                     ckb,
                     ldk,
                 } => (
+                    // Successfully read config file, merging these options with the default ones.
                     ckb.map(|c| CkbConfig::from(c).merge(&mut args.ckb)),
                     ldk.map(|c| LdkConfig::from(c).merge(&mut args.ldk)),
                 ),
@@ -146,6 +160,7 @@ impl Config {
                 ldk.unwrap_or(LdkConfig::from(&mut args.ldk)),
             ),
         };
+
         let ckb = if services.contains(&Service::CKB) {
             Some(ckb)
         } else {
