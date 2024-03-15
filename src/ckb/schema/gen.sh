83df5833cb88fbd11b289dd07c450e35de4dd1d6
YI <uuuuuu@protonmail.com>
2024-03-15 07:57:57 +0000

    Import pcn message definition

---
+++ src/ckb/schema/gen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+MOLC="${MOLC:-moleculec}"
+
+schema_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+gen_dir="$(dirname "$schema_dir")/gen"
+
+for f in "$schema_dir"/pcn.mol; do
+    output_file="$gen_dir/$(basename "${f%%.mol}.rs")"
+    "$MOLC" --language rust --schema-file "$f" | rustfmt > "$output_file"
+done
