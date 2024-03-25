d7b41a2a15e67a6e0ee8c2d98f38eaa1216dbf01
YI <uuuuuu@protonmail.com>
2024-03-25 14:26:14 +0000

    Process command from http request

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use log::{debug, info};
+use log::{debug, error, info};
 use tentacle::multiaddr::Multiaddr;
+use tokio::signal;
 use tokio::sync::mpsc;
-use tokio::{select, signal};
 use tokio_util::sync::CancellationToken;
 use tokio_util::task::task_tracker::TaskTracker;
 
 use std::str::FromStr;
 
 use ckb_pcn_node::ckb::Command;
-use ckb_pcn_node::{start_ckb, start_ldk, start_rpc, CkbConfig, Config};
+use ckb_pcn_node::{start_ckb, start_ldk, start_rpc, Config};
 
 #[derive(Debug, Clone)]
 struct TaskTrackerWithCancellation {
@@ -44,49 +44,6 @@ fn new_tokio_task_tracker() -> TaskTracker {
     TOKIO_TASK_TRACKER_WITH_CANCELLATION.tracker.clone()
 }
 
-async fn run_ckb(ckb_config: CkbConfig) {
-    const CHANNEL_SIZE: usize = 4000;
-    let (command_sender, command_receiver) = mpsc::channel(CHANNEL_SIZE);
-    info!("Starting ckb");
-
-    assert!(
-        ckb_config.bootnode_addrs.len() < CHANNEL_SIZE,
-        "Too many bootnodes ({} allowed, having {})",
-        CHANNEL_SIZE,
-        ckb_config.bootnode_addrs.len()
-    );
-    for bootnode in &ckb_config.bootnode_addrs {
-        let addr = Multiaddr::from_str(bootnode).expect("valid bootnode");
-        let command = Command::Connect(addr);
-        command_sender
-            .send(command)
-            .await
-            .expect("receiver not closed")
-    }
-
-    new_tokio_task_tracker().spawn(async move {
-        let token = new_tokio_cancellation_token();
-        let _command_sender: mpsc::Sender<ckb_pcn_node::ckb::Command> = command_sender;
-        loop {
-            select! {
-                _ = token.cancelled() => {
-                    debug!("Dropping command sender as tokio cancelled");
-                    break;
-                }
-                // TODO: Do some work related to command sender here,
-            }
-        }
-    });
-
-    start_ckb(
-        ckb_config,
-        command_receiver,
-        new_tokio_cancellation_token(),
-        new_tokio_task_tracker(),
-    )
-    .await;
-}
-
 #[tokio::main]
 pub async fn main() {
     env_logger::init();
@@ -100,17 +57,52 @@ pub async fn main() {
                 info!("Starting ldk");
                 start_ldk(ldk_config).await;
             }
-            if let Some(ckb_config) = ckb {
-                run_ckb(ckb_config).await;
-            }
-            if let Some(rpc_config) = rpc {
-                let shutdown_signal = async {
-                    let token = new_tokio_cancellation_token();
-                    token.cancelled().await;
-                };
-                new_tokio_task_tracker().spawn(async move {
-                    start_rpc(rpc_config, shutdown_signal).await;
-                });
+            match (ckb, rpc) {
+                (None, Some(_)) => {
+                    error!("Rpc service requires ckb service to be started. Exiting.");
+                    return;
+                }
+                (None, None) => {
+                    // No service to run, skipping.
+                }
+                (Some(ckb_config), rpc) => {
+                    const CHANNEL_SIZE: usize = 4000;
+                    let (command_sender, command_receiver) = mpsc::channel(CHANNEL_SIZE);
+                    assert!(
+                        ckb_config.bootnode_addrs.len() < CHANNEL_SIZE,
+                        "Too many bootnodes ({} allowed, having {})",
+                        CHANNEL_SIZE,
+                        ckb_config.bootnode_addrs.len()
+                    );
+                    for bootnode in &ckb_config.bootnode_addrs {
+                        let addr = Multiaddr::from_str(bootnode).expect("valid bootnode");
+                        let command = Command::Connect(addr);
+                        command_sender
+                            .send(command)
+                            .await
+                            .expect("receiver not closed")
+                    }
+
+                    info!("Starting ckb");
+                    start_ckb(
+                        ckb_config,
+                        command_receiver,
+                        new_tokio_cancellation_token(),
+                        new_tokio_task_tracker(),
+                    )
+                    .await;
+
+                    // Start rpc service
+                    if let Some(rpc_config) = rpc {
+                        let shutdown_signal = async {
+                            let token = new_tokio_cancellation_token();
+                            token.cancelled().await;
+                        };
+                        new_tokio_task_tracker().spawn(async move {
+                            start_rpc(rpc_config, command_sender, shutdown_signal).await;
+                        });
+                    };
+                }
             }
         }
     }
--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -1,29 +1,59 @@
 mod config;
 pub use config::RpcConfig;
 
-use axum::{http::StatusCode, response::IntoResponse, routing::post, Json, Router};
-use log::debug;
+use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::post, Json, Router};
+use log::{debug, info};
 use serde::Deserialize;
-use std::future::Future;
+use std::{future::Future, sync::Arc};
+use tokio::sync::mpsc;
 
 use crate::ckb::Command;
 
 #[derive(Debug, Deserialize)]
-pub struct HttpRequest {
-    pub id: Option<u64>,
-    pub command: Command,
+pub enum HttpRequest {
+    Command(Command),
 }
 
-async fn handle_request(Json(http_request): Json<HttpRequest>) -> impl IntoResponse {
+#[derive(Debug, Deserialize)]
+pub struct HttpBody {
+    pub id: Option<u64>,
+    pub request: HttpRequest,
+}
+
+pub struct AppState {
+    pub ckb_command_sender: mpsc::Sender<Command>,
+}
+
+async fn handle_request(
+    State(state): State<Arc<AppState>>,
+    Json(http_request): Json<HttpBody>,
+) -> impl IntoResponse {
     debug!("Recevied http request: {:?}", http_request);
+    match http_request {
+        HttpBody { id: _, request } => match request {
+            HttpRequest::Command(command) => {
+                state
+                    .ckb_command_sender
+                    .send(command)
+                    .await
+                    .expect("send command");
+            }
+        },
+    }
     StatusCode::OK
 }
 
-pub async fn start_rpc<F>(config: RpcConfig, shutdown_signal: F)
-where
+pub async fn start_rpc<F>(
+    config: RpcConfig,
+    ckb_command_sender: mpsc::Sender<Command>,
+    shutdown_signal: F,
+) where
     F: Future<Output = ()> + Send + 'static,
 {
-    let app = Router::new().route("/", post(handle_request));
+    let app_state = Arc::new(AppState { ckb_command_sender });
+    let app = Router::new()
+        .route("/", post(handle_request))
+        .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind(config.listening_addr)
         .await
