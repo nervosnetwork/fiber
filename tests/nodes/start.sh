0d9e0b30a3ee4692bfd5563a9bc2c239f1fe9046
YI <uuuuuu@protonmail.com>
2024-03-26 17:09:38 +0800

    Add bruno and script to start 3 nodes and test api

---
+++ tests/nodes/start.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export RUST_BACKTRACE=full RUST_LOG=debug
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+nodes_dir="$(dirname "$script_dir")/nodes"
+
+cd "$nodes_dir" || exit 1
+
+start() {
+    cargo run -- "$@"
+}
+
+start -d 1 &
+start -d 2 &
+start -d 3 &
+
+wait
