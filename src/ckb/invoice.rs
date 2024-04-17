0af5dc8882bc5f7d6fa5b1cb2656b9f33b5ba2d7
yukang <moorekang@gmail.com>
2024-04-18 00:16:30 +0800

    add invoice attributes

--- src/ckb/invoice.rs
+++ src/ckb/invoice.rs
@@ -1,20 +1,13 @@
-use ckb_types::packed::{BytesOpt, BytesOptBuilder};
-use molecule::prelude::{Builder, Entity};
-
-use super::gen::pcn::{self as molecule_pcn};
-use crate::ckb::gen::pcn::*;
-
-use core::time::Duration;
-
-use serde::{Deserialize, Serialize};
-use serde_with::base64::Base64;
-use serde_with::serde_as;
-use thiserror::Error;
-
-use ckb_types::{
-    packed::{Byte32, BytesVec, Script, Transaction},
-    prelude::{Pack, Unpack},
+use super::gen::{
+    invoice as molecule_invoice,
+    invoice::{RawCkbInvoice, RawCkbInvoiceBuilder},
 };
+use crate::ckb::gen::invoice::*;
+use ckb_types::prelude::{Pack, Unpack};
+use core::time::Duration;
+use molecule::prelude::{Builder, Entity};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
@@ -22,7 +15,7 @@ pub enum Error {
     Molecule(#[from] molecule::error::VerificationError),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Serialize, Deserialize)]
 pub enum Currency {
     Ckb,
     CkbTestNet,
@@ -31,14 +24,14 @@ pub enum Currency {
 impl From<u8> for Currency {
     fn from(byte: u8) -> Self {
         match byte {
-            1 => Self::Ckb,
-            2 => Self::CkbTestNet,
+            0 => Self::Ckb,
+            1 => Self::CkbTestNet,
             _ => panic!("Invalid value for Currency"),
         }
     }
 }
 
-#[derive(Eq, PartialEq, Debug, Clone, Copy, Hash, Ord, PartialOrd)]
+#[derive(Eq, PartialEq, Debug, Clone, Copy, Hash, Ord, PartialOrd, Serialize, Deserialize)]
 pub enum SiPrefix {
     /// 10^-3
     Milli,
@@ -53,35 +46,107 @@ pub enum SiPrefix {
 impl From<u8> for SiPrefix {
     fn from(byte: u8) -> Self {
         match byte {
-            1 => Self::Milli,
-            2 => Self::Micro,
-            3 => Self::Nano,
-            4 => Self::Pico,
+            0 => Self::Milli,
+            1 => Self::Micro,
+            2 => Self::Nano,
+            3 => Self::Pico,
             _ => panic!("Invalid value for SiPrefix"),
         }
     }
 }
 
-pub struct InvoiceBuilder {
-    currency: Currency,
-    amount: Option<u64>,
-    prefix: Option<SiPrefix>,
-    description: Option<String>,
+#[derive(Eq, PartialEq, Debug, Clone)]
+pub enum Attribute {
+    FinalHtlcTimeout(u64),
+    FinalHtlcMinimumCltvExpiry(u64),
+    ExpiryTime(Duration),
+    Description(String),
+    FallbackAddr(String),
+    Feature(u64),
 }
 
-#[derive(Debug, Clone)]
-pub struct RawCkbInvoice {
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct InvoiceData {
+    pub payment_hash: [u8; 32],
+    pub signature: [u8; 64],
+    pub attrs: Vec<Attribute>,
+}
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct CkbInvoice {
     pub currency: Currency,
     pub amount: Option<u64>,
     pub prefix: Option<SiPrefix>,
-    //pub payment_hash: Option<Bytes>,
-    pub description: Option<String>,
-    pub expiry_time: Option<Duration>,
+    pub data: InvoiceData,
 }
 
-impl From<RawCkbInvoice> for molecule_pcn::RawCkbInvoice {
-    fn from(invoice: RawCkbInvoice) -> Self {
-        molecule_pcn::RawCkbInvoiceBuilder::default()
+impl From<Attribute> for InvoiceAttr {
+    fn from(attr: Attribute) -> Self {
+        let a = match attr {
+            Attribute::ExpiryTime(x) => {
+                let seconds = x.as_secs();
+                let nanos = x.subsec_nanos() as u64;
+                let value = molecule_invoice::Duration::new_builder()
+                    .seconds(seconds.pack())
+                    .nanos(nanos.pack())
+                    .build();
+                InvoiceAttrUnion::ExpiryTime(ExpiryTime::new_builder().value(value).build())
+            }
+            Attribute::Description(value) => InvoiceAttrUnion::Description(
+                Description::new_builder().value(value.pack()).build(),
+            ),
+            Attribute::FinalHtlcTimeout(value) => InvoiceAttrUnion::FinalHtlcTimeout(
+                FinalHtlcTimeout::new_builder().value(value.pack()).build(),
+            ),
+            Attribute::FinalHtlcMinimumCltvExpiry(value) => {
+                InvoiceAttrUnion::FinalHtlcMinimumCltvExpiry(
+                    FinalHtlcMinimumCltvExpiry::new_builder()
+                        .value(value.pack())
+                        .build(),
+                )
+            }
+            Attribute::FallbackAddr(value) => InvoiceAttrUnion::FallbackAddr(
+                FallbackAddr::new_builder().value(value.pack()).build(),
+            ),
+            Attribute::Feature(value) => {
+                InvoiceAttrUnion::Feature(Feature::new_builder().value(value.pack()).build())
+            }
+        };
+        InvoiceAttr::new_builder().set(a).build()
+    }
+}
+
+impl From<InvoiceAttr> for Attribute {
+    fn from(attr: InvoiceAttr) -> Self {
+        match attr.to_enum() {
+            InvoiceAttrUnion::Description(x) => {
+                let value: Vec<u8> = x.value().unpack();
+                Attribute::Description(String::from_utf8(value).unwrap())
+            }
+            InvoiceAttrUnion::ExpiryTime(x) => {
+                let seconds: u64 = x.value().seconds().unpack();
+                let nanos: u64 = x.value().nanos().unpack();
+                Attribute::ExpiryTime(
+                    Duration::from_secs(seconds).saturating_add(Duration::from_nanos(nanos)),
+                )
+            }
+            InvoiceAttrUnion::FinalHtlcTimeout(x) => {
+                Attribute::FinalHtlcTimeout(x.value().unpack())
+            }
+            InvoiceAttrUnion::FinalHtlcMinimumCltvExpiry(x) => {
+                Attribute::FinalHtlcMinimumCltvExpiry(x.value().unpack())
+            }
+            InvoiceAttrUnion::FallbackAddr(x) => {
+                let value: Vec<u8> = x.value().unpack();
+                Attribute::FallbackAddr(String::from_utf8(value).unwrap())
+            }
+            InvoiceAttrUnion::Feature(x) => Attribute::Feature(x.value().unpack()),
+        }
+    }
+}
+
+impl From<CkbInvoice> for RawCkbInvoice {
+    fn from(invoice: CkbInvoice) -> Self {
+        RawCkbInvoiceBuilder::default()
             .currency((invoice.currency as u8).into())
             .amount(
                 AmountOpt::new_builder()
@@ -93,105 +158,57 @@ impl From<RawCkbInvoice> for molecule_pcn::RawCkbInvoice {
                     .set(invoice.prefix.map(|x| (x as u8).into()))
                     .build(),
             )
-            //.payment_hash(invoice.payment_hash)
-            .description(
-                BytesOpt::new_builder()
+            .data(InvoiceData::from(invoice.data).into())
+            .build()
+    }
+}
+
+impl TryFrom<molecule_invoice::RawCkbInvoice> for CkbInvoice {
+    type Error = Error;
+
+    fn try_from(invoice: molecule_invoice::RawCkbInvoice) -> Result<Self, Self::Error> {
+        Ok(CkbInvoice {
+            currency: (u8::from(invoice.currency())).into(),
+            amount: invoice.amount().to_opt().map(|x| x.unpack()),
+            prefix: invoice.prefix().to_opt().map(|x| u8::from(x).into()),
+            data: invoice.data().try_into()?,
+        })
+    }
+}
+
+impl From<InvoiceData> for molecule_invoice::RawInvoiceData {
+    fn from(data: InvoiceData) -> Self {
+        RawInvoiceDataBuilder::default()
+            .payment_hash(PaymentHash::from(data.payment_hash))
+            .signature(Signature::from(data.signature))
+            .attrs(
+                InvoiceAttrsVec::new_builder()
                     .set(
-                        invoice
-                            .description
-                            .map(|x| molecule::bytes::Bytes::from(x.into_bytes()).pack()),
+                        data.attrs
+                            .iter()
+                            .map(|a| a.to_owned().into())
+                            .collect::<Vec<InvoiceAttr>>(),
                     )
-                    .build(),
-            )
-            .expiry_time(
-                ExpiryTimeOpt::new_builder()
-                    .set(invoice.expiry_time.map(|x| {
-                        let seconds = x.as_secs();
-                        let nanos = x.subsec_nanos() as u64;
-                        molecule_pcn::Duration::new_builder()
-                            .seconds(seconds.pack())
-                            .nanos(nanos.pack())
-                            .build()
-                    }))
                     .build(),
             )
             .build()
     }
 }
 
-impl TryFrom<molecule_pcn::RawCkbInvoice> for RawCkbInvoice {
+impl TryFrom<molecule_invoice::RawInvoiceData> for InvoiceData {
     type Error = Error;
 
-    fn try_from(invoice: molecule_pcn::RawCkbInvoice) -> Result<Self, Self::Error> {
-        Ok(RawCkbInvoice {
-            currency: (u8::from(invoice.currency())).into(),
-            amount: invoice.amount().to_opt().map(|x| x.unpack()),
-            prefix: invoice
-                .prefix()
-                .to_opt()
-                .map(|x| u8::from(x))
-                .map(|x| x.into()),
-            //payment_hash: invoice.payment_hash().to_opt().map(|x| x.unpack()),
-            description: invoice.description().to_opt().map(|x| {
-                let v: Vec<u8> = x.unpack();
-                String::from_utf8(v.to_vec()).unwrap()
-            }),
-            expiry_time: invoice.expiry_time().to_opt().map(|x| {
-                let seconds: u64 = x.seconds().unpack();
-                let nanos: u64 = x.nanos().unpack();
-                core::time::Duration::from_secs(seconds)
-                    .saturating_add(core::time::Duration::from_nanos(nanos))
-            }),
+    fn try_from(data: molecule_invoice::RawInvoiceData) -> Result<Self, Self::Error> {
+        Ok(InvoiceData {
+            payment_hash: data.payment_hash().into(),
+            signature: data.signature().into(),
+            attrs: data
+                .attrs()
+                .into_iter()
+                .map(|a| a.into())
+                .collect::<Vec<Attribute>>(),
         })
     }
-}
-
-impl InvoiceBuilder {
-    pub fn new() -> Self {
-        Self {
-            currency: Currency::Ckb,
-            amount: None,
-            prefix: None,
-            description: None,
-        }
-    }
-
-    pub fn currency(mut self, currency: Currency) -> Self {
-        self.currency = currency;
-        self
-    }
-
-    pub fn amount(mut self, amount: u64) -> Self {
-        self.amount = Some(amount);
-        self
-    }
-
-    pub fn prefix(mut self, prefix: SiPrefix) -> Self {
-        self.prefix = Some(prefix);
-        self
-    }
-
-    pub fn description(mut self, description: String) -> Self {
-        self.description = Some(description);
-        self
-    }
-
-    // pub fn build(self) -> RawCkbInvoice {
-    //     let desc = self.description.unwrap().as_bytes();
-    //     let desc_data: ckb_types::packed::Bytes = ckb_types::packed::Bytes::new_builder()
-    //         .set(desc.to_vec())
-    //         .build();
-    //     let mut data = RawDataPartBuilder::default()
-    //         .description(BytesOptBuilder::default().set(Some(desc_data)).build())
-    //         .build();
-    //     // let mut invoice = RawCkbInvoiceBuilder::default();
-    //     // let data = RawCkbInvoiceData::default();
-    //     // let mut expiry_time = RawCkbInvoiceExpiryTime::default();
-    //     // expiry_time.set_duration(Duration::from_secs(0).into());
-    //     // data.set_expiry_time(expiry_time);
-    //     // invoice.set_data(data);
-    //     // invoice
-    // }
 }
 
 // write test for invoices
@@ -199,13 +216,52 @@ impl InvoiceBuilder {
 mod tests {
     use super::*;
 
+    use bech32::{decode, encode, FromBase32, ToBase32, Variant};
+    fn mock_invoice() -> CkbInvoice {
+        CkbInvoice {
+            currency: Currency::Ckb,
+            amount: Some(1280),
+            prefix: Some(SiPrefix::Milli),
+            data: InvoiceData {
+                payment_hash: [0u8; 32],
+                signature: [0u8; 64],
+                attrs: vec![
+                    Attribute::FinalHtlcTimeout(5),
+                    Attribute::FinalHtlcMinimumCltvExpiry(12),
+                    Attribute::Description("description".to_string()),
+                    Attribute::ExpiryTime(Duration::from_secs(1024)),
+                    Attribute::FallbackAddr("address".to_string()),
+                ],
+            },
+        }
+    }
     #[test]
-    fn test_create_invoice() {
-        // let invoice = RawCkbInvoiceBuilder::default().build();
-        // let data = invoice.data();
-        // let duration = data.expiry_time().to_opt();
-        // assert_eq!(duration.is_none(), true);
-        // eprintln!("invoice: {}", invoice.data());
-        // eprintln!("invoice: {}", data.expiry_time());
+    fn test_ckb_invoice() {
+        let ckb_invoice = mock_invoice();
+        let ckb_invoice_backup = ckb_invoice.clone();
+
+        let invoice_copy: RawCkbInvoice = ckb_invoice.into();
+        let ckb_invoice_rec: CkbInvoice = invoice_copy.try_into().unwrap();
+
+        assert_eq!(ckb_invoice_rec, ckb_invoice_backup);
+    }
+
+    #[test]
+    fn test_invoice_bc32m() {
+        let invoice = mock_invoice();
+        let raw_invoice = RawCkbInvoice::from(invoice.clone());
+        let slice = raw_invoice.as_slice();
+        let mut base32 = Vec::with_capacity(slice.len());
+        slice.write_base32(&mut base32).unwrap();
+        let result = encode("hrp", base32, Variant::Bech32).unwrap();
+        eprintln!("invoice: {}", result);
+        eprintln!("invoice_len: {:?}", result.len());
+        let (msg, data, _) = decode(&result).unwrap();
+        assert_eq!(msg, "hrp".to_string());
+        // convert from base32 to slice
+        let data = Vec::<u8>::from_base32(&data).unwrap();
+        let slice = RawCkbInvoice::from_slice(&data).unwrap();
+        let decoded_invoice: CkbInvoice = slice.try_into().unwrap();
+        assert_eq!(decoded_invoice, invoice);
     }
 }
--- src/ckb/schema/gen.sh
+++ src/ckb/schema/gen.sh
@@ -7,7 +7,9 @@ MOLC="${MOLC:-moleculec}"
 schema_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 gen_dir="$(dirname "$schema_dir")/gen"
 
-for f in "$schema_dir"/pcn.mol; do
+files=("pcn.mol" "invoice.mol")
+for file in "${files[@]}"; do
+    f="$schema_dir/$file"
     output_file="$gen_dir/$(basename "${f%%.mol}.rs")"
     "$MOLC" --language rust --schema-file "$f" | rustfmt > "$output_file"
 done
