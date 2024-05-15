f15a0639594eadef7c3f8b54d63edf970b4a8c49
YI <uuuuuu@protonmail.com>
2024-05-16 01:38:01 +0800

    Enable IntegrationTest for dev ckb node

--- tests/deploy/run-dev-node.sh
+++ tests/deploy/run-dev-node.sh
@@ -27,7 +27,16 @@ done
 
 if ! [[ -d "$data_dir" ]]; then
     ckb init -C "$data_dir" -c dev --force --ba-arg 0xc8328aabcd9b9e8e64fbc566c4385c3bdeb219d7
+    # Enable the IntegrationTest module (required to generate blocks).
+    if ! grep -E '^modules.*IntegrationTest' "$data_dir/ckb.toml"; then
+        sed -E -i 's/^(modules.*)\[(.*)\]/\1[\2,"IntegrationTest"]/' "$data_dir/ckb.toml"
+    fi
     # Transfer some money from the default account (node 3) to node 1 for later use.
-    (sleep 3; ckb-cli wallet transfer --to-address ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqgx5lf4pczpamsfam48evs0c8nvwqqa59qapt46f --capacity 5000000000 --fee-rate 2000 --privkey-path "$script_dir/../nodes/3/ckb-chain/key") &
+    (
+        sleep 3
+        ckb-cli wallet transfer --to-address ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqgx5lf4pczpamsfam48evs0c8nvwqqa59qapt46f --capacity 5000000000 --fee-rate 2000 --privkey-path "$script_dir/../nodes/3/ckb-chain/key"
+        # Generate a few blocks so that above transaction is confirmed.
+        "$script_dir/generate-blocks.sh" 4
+    ) &
 fi
 ckb run -C "$data_dir" --indexer
