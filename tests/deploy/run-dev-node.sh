968a4df5b5787406fbf1f35b5387d3b8f6ff7016
yukang <moorekang@gmail.com>
2024-05-16 21:56:15 +0800

    fix 3-nodes demo

--- tests/deploy/run-dev-node.sh
+++ tests/deploy/run-dev-node.sh
@@ -29,12 +29,14 @@ if ! [[ -d "$data_dir" ]]; then
     ckb init -C "$data_dir" -c dev --force --ba-arg 0xc8328aabcd9b9e8e64fbc566c4385c3bdeb219d7
     # Enable the IntegrationTest module (required to generate blocks).
     if ! grep -E '^modules.*IntegrationTest' "$data_dir/ckb.toml"; then
-        sed -i '' 's/\("Debug"\)/\1, "IntegrationTest"/' "$data_dir"/ckb.toml
+        sed -i.bak 's/\("Debug"\)/\1, "IntegrationTest"/' "$data_dir"/ckb.toml
     fi
-    # Transfer some money from the default account (node 3) to node 1 for later use.
+    # Transfer some money from the default account (node 3) to node 1 and node 2 for later use.
     (
         sleep 3
         ckb-cli wallet transfer --to-address ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqgx5lf4pczpamsfam48evs0c8nvwqqa59qapt46f --capacity 5000000000 --fee-rate 2000 --privkey-path "$script_dir/../nodes/3/ckb-chain/key"
+        sleep 3
+        ckb-cli wallet transfer --to-address ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqt4vqqyehpxn47deg5l6eeqtkfrt5kfkfchkwv62 --capacity 5000000000 --fee-rate 2000 --privkey-path "$script_dir/../nodes/3/ckb-chain/key"
         # Generate a few blocks so that above transaction is confirmed.
         "$script_dir/generate-blocks.sh" 4
     ) &
