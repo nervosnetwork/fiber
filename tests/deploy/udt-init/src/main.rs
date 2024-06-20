d83d69175c31a4b50305fba24cd619930e23c4b6
yukang <moorekang@gmail.com>
2024-06-20 22:59:32 +0800

    add xudt for e2e test

--- tests/deploy/udt-init/src/main.rs
+++ tests/deploy/udt-init/src/main.rs
@@ -22,8 +22,19 @@ use serde_yaml;
 use std::{error::Error as StdErr, str::FromStr};
 
 const SIMPLE_CODE_HASH: H256 =
+    h256!("0xe1e354d6d643ad42724d40967e334984534e0367405c5ae42a9d7d63d77df419");
+const XUDT_CODE_HASH: H256 =
     h256!("0x50bd8d6680b8b9cf98b73f3c08faf8b2a21914311954118ad6609be6e78a1b95");
-const CKB_SHANNONS: u64 = 100_000_000;
+
+const UDT_KINDS: [&str; 2] = ["SIMPLE_UDT", "XUDT"];
+
+fn get_code_hash(udt_kind: &str) -> H256 {
+    match udt_kind {
+        "SIMPLE_UDT" => SIMPLE_CODE_HASH.clone(),
+        "XUDT" => XUDT_CODE_HASH.clone(),
+        _ => panic!("unsupported udt kind"),
+    }
+}
 
 fn get_env_hex(name: &str) -> H256 {
     let value = std::env::var(name).expect("env var");
@@ -32,15 +43,15 @@ fn get_env_hex(name: &str) -> H256 {
     H256::from_str(&value).expect("parse hex")
 }
 
-fn gen_dev_udt_handler() -> SudtHandler {
-    let simple_udt_tx = get_env_hex("NEXT_PUBLIC_XUDT_TX_HASH");
-
+fn gen_dev_udt_handler(udt_kind: &str) -> SudtHandler {
+    let udt_tx = get_env_hex(format!("NEXT_PUBLIC_{}_TX_HASH", udt_kind).as_str());
+    let code_hash = get_code_hash(udt_kind);
     let (out_point, script_id) = (
         OutPoint::new_builder()
-            .tx_hash(simple_udt_tx.pack())
+            .tx_hash(udt_tx.pack())
             .index(0u32.pack())
             .build(),
-        ScriptId::new_data1(SIMPLE_CODE_HASH.clone()),
+        ScriptId::new_data1(code_hash),
     );
 
     let cell_dep = CellDep::new_builder()
@@ -48,11 +59,7 @@ fn gen_dev_udt_handler() -> SudtHandler {
         .dep_type(DepType::Code.into())
         .build();
 
-    let udt_handler = ckb_sdk::transaction::handler::sudt::SudtHandler::new_with_customize(
-        vec![cell_dep],
-        script_id,
-    );
-    return udt_handler;
+    ckb_sdk::transaction::handler::sudt::SudtHandler::new_with_customize(vec![cell_dep], script_id)
 }
 
 fn gen_dev_sighash_handler() -> Secp256k1Blake160SighashAllScriptHandler {
@@ -68,30 +75,30 @@ fn gen_dev_sighash_handler() -> Secp256k1Blake160SighashAllScriptHandler {
         .dep_type(DepType::DepGroup.into())
         .build();
 
-    let sighash_handler =
-        Secp256k1Blake160SighashAllScriptHandler::new_with_customize(vec![cell_dep]);
-    return sighash_handler;
+    Secp256k1Blake160SighashAllScriptHandler::new_with_customize(vec![cell_dep])
 }
 
 fn generate_configuration(
+    udt_kind: &str,
 ) -> Result<(NetworkInfo, TransactionBuilderConfiguration), Box<dyn StdErr>> {
     let network_info = NetworkInfo::devnet();
     let mut configuration =
         TransactionBuilderConfiguration::new_devnet().expect("new devnet configuration");
 
     configuration.register_script_handler(Box::new(gen_dev_sighash_handler()));
-    configuration.register_script_handler(Box::new(gen_dev_udt_handler()));
+    configuration.register_script_handler(Box::new(gen_dev_udt_handler(udt_kind)));
     return Ok((network_info, configuration));
 }
 
 fn init_or_send_udt(
+    udt_kind: &str,
     issuer_address: &str,
     sender_info: &(String, H256),
     receiver_address: Option<&str>,
     sudt_amount: u128,
     apply: bool,
 ) -> Result<(), Box<dyn StdErr>> {
-    let (network_info, configuration) = generate_configuration()?;
+    let (network_info, configuration) = generate_configuration(udt_kind)?;
 
     let issuer = Address::from_str(issuer_address)?;
     let sender = Address::from_str(&sender_info.0)?;
@@ -104,6 +111,7 @@ fn init_or_send_udt(
     let iterator = InputIterator::new_with_address(&[sender.clone()], &network_info);
     let owner_mode = receiver_address.is_none();
     let mut builder = SudtTransactionBuilder::new(configuration, iterator, &issuer, owner_mode)?;
+    builder.set_sudt_type_script(generate_udt_type_script(udt_kind, issuer_address));
     builder.add_output(&receiver, sudt_amount);
 
     let mut tx_with_groups = builder.build(&Default::default())?;
@@ -133,22 +141,6 @@ fn init_or_send_udt(
     Ok(())
 }
 
-fn check_account(address: &str, issuer_address: &str) -> Result<(), Box<dyn StdErr>> {
-    let (network_info, configuration) = generate_configuration()?;
-    let issuer = Address::from_str(issuer_address)?;
-    let sender = Address::from_str(address)?;
-    let iterator = InputIterator::new_with_address(&[sender.clone()], &network_info);
-    let builder = SudtTransactionBuilder::new(configuration, iterator, &issuer, false)?;
-
-    let (account_ckb_amount, account_udt_amount) = builder.check()?;
-    eprintln!(
-        "account: {:?} udt_amount: {}",
-        account_ckb_amount / CKB_SHANNONS,
-        account_udt_amount
-    );
-    Ok(())
-}
-
 fn generate_blocks(num: u64) -> Result<(), Box<dyn StdErr>> {
     let network_info = NetworkInfo::devnet();
     let rpc_client = CkbRpcClient::new(network_info.url.as_str());
@@ -161,16 +153,15 @@ fn generate_blocks(num: u64) -> Result<(), Box<dyn StdErr>> {
     Ok(())
 }
 
-fn generate_udt_type_script(address: &str) -> ckb_types::packed::Script {
+fn generate_udt_type_script(udt_kind: &str, address: &str) -> ckb_types::packed::Script {
     let address = Address::from_str(address).expect("parse address");
     let sudt_owner_lock_script: Script = (&address).into();
-
-    let res = Script::new_builder()
-        .code_hash(SIMPLE_CODE_HASH.pack())
+    let code_hash = get_code_hash(udt_kind);
+    Script::new_builder()
+        .code_hash(code_hash.pack())
         .hash_type(ScriptHashType::Data1.into())
         .args(sudt_owner_lock_script.calc_script_hash().as_bytes().pack())
-        .build();
-    res
+        .build()
 }
 
 fn get_nodes_info(node: &str) -> (String, H256) {
@@ -214,19 +205,23 @@ fn genrate_nodes_config() {
     let yaml_file_path = format!("{}/config.yml", nodes_dir);
     let content = std::fs::read_to_string(yaml_file_path).expect("read failed");
     let data: serde_yaml::Value = serde_yaml::from_str(&content).expect("Unable to parse YAML");
-    let udt_info = UdtInfo {
-        name: "simple_udt".to_string(),
-        script: UdtScript {
-            code_hash: SIMPLE_CODE_HASH.clone(),
-            hash_type: "Data1".to_string(),
-            args: "0x.*".to_string(),
-        },
-        cell_deps: vec![UdtCellDep {
-            dep_type: "code".to_string(),
-            tx_hash: get_env_hex("NEXT_PUBLIC_XUDT_TX_HASH"),
-            index: 0,
-        }],
-    };
+    let mut udt_infos = vec![];
+    for udt in UDT_KINDS {
+        let udt_info = UdtInfo {
+            name: udt.to_string(),
+            script: UdtScript {
+                code_hash: get_code_hash(udt),
+                hash_type: "Data1".to_string(),
+                args: "0x.*".to_string(),
+            },
+            cell_deps: vec![UdtCellDep {
+                dep_type: "code".to_string(),
+                tx_hash: get_env_hex(format!("NEXT_PUBLIC_{}_TX_HASH", udt).as_str()),
+                index: 0,
+            }],
+        };
+        udt_infos.push(udt_info);
+    }
 
     let header = "# generated from nodes/config.yml, don't edit it manually\n";
     for i in 1..=3 {
@@ -236,7 +231,7 @@ fn genrate_nodes_config() {
         data["ckb"]["annouced_name"] = serde_yaml::Value::String(format!("ckb-{}", i));
         data["rpc"]["listening_addr"] =
             serde_yaml::Value::String(format!("127.0.0.1:{}", 41714 + i - 1));
-        data["ckb_chain"]["udt_whitelist"] = serde_yaml::to_value(vec![&udt_info]).unwrap();
+        data["ckb_chain"]["udt_whitelist"] = serde_yaml::to_value(&udt_infos).unwrap();
         let new_yaml = header.to_string() + &serde_yaml::to_string(&data).unwrap();
         let config_path = format!("{}/{}/config.yml", nodes_dir, i);
         std::fs::write(config_path, new_yaml).expect("write failed");
@@ -245,49 +240,27 @@ fn genrate_nodes_config() {
 
 fn init_udt_accounts() -> Result<(), Box<dyn StdErr>> {
     let udt_owner = get_nodes_info("deployer");
-    let wallets = [
-        get_nodes_info("1"),
-        get_nodes_info("2"),
-        get_nodes_info("3"),
-    ];
+    for udt in UDT_KINDS {
+        init_or_send_udt(udt, &udt_owner.0, &udt_owner, None, 1000000000000, true)
+            .expect("init udt");
+        generate_blocks(4).expect("ok");
 
-    init_or_send_udt(&udt_owner.0, &udt_owner, None, 1000000000000, true).expect("init udt");
-    generate_blocks(4).expect("ok");
+        for i in 0..3 {
+            let wallet = get_nodes_info(&(i + 1).to_string());
+            init_or_send_udt(
+                udt,
+                &udt_owner.0,
+                &udt_owner,
+                Some(&wallet.0),
+                200000000000,
+                true,
+            )?;
+            generate_blocks(4).expect("ok");
+        }
 
-    init_or_send_udt(
-        &udt_owner.0,
-        &udt_owner,
-        Some(&wallets[0].0),
-        200000000000,
-        true,
-    )?;
-    generate_blocks(4).expect("ok");
-
-    init_or_send_udt(
-        &udt_owner.0,
-        &udt_owner,
-        Some(&wallets[1].0),
-        200000000000,
-        true,
-    )?;
-    generate_blocks(4).expect("ok");
-
-    init_or_send_udt(
-        &udt_owner.0,
-        &udt_owner,
-        Some(&wallets[2].0),
-        200000000000,
-        true,
-    )?;
-    generate_blocks(4).expect("ok");
-
-    check_account(&udt_owner.0, &udt_owner.0)?;
-    check_account(&wallets[0].0, &udt_owner.0)?;
-    check_account(&wallets[1].0, &udt_owner.0)?;
-    check_account(&wallets[2].0, &udt_owner.0)?;
-
-    let script = generate_udt_type_script(&udt_owner.0);
-    println!("initialized udt_type_script: {} ...", script);
+        let script = generate_udt_type_script(udt, &udt_owner.0);
+        println!("initialized udt_type_script: {} ...", script);
+    }
     Ok(())
 }
 
