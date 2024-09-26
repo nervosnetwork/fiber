b61ff8c3007f16812aca50f7dfce34130d8e5d40
YI <uuuuuu@protonmail.com>
2024-09-26 18:39:04 +0000

    Add test for add ChannelUpdate version

--- src/fiber/network.rs
+++ src/fiber/network.rs
@@ -3464,12 +3464,12 @@ pub async fn start_network<
 mod tests {
     use crate::{
         fiber::{
-            graph::NetworkGraphStateStore,
+            graph::{ChannelInfo, NetworkGraphStateStore},
             network::{get_chain_hash, PeerId},
             test_utils::{init_tracing, NetworkNode},
             types::{
-                ChannelAnnouncement, FiberBroadcastMessage, FiberMessage, NodeAnnouncement,
-                Privkey, Pubkey,
+                ChannelAnnouncement, ChannelUpdate, FiberBroadcastMessage, FiberMessage,
+                NodeAnnouncement, Privkey, Pubkey,
             },
             NetworkActorCommand, NetworkActorMessage,
         },
@@ -3625,6 +3625,142 @@ mod tests {
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
         let channels = node2.store.get_channels(None);
         assert!(!channels.is_empty());
+    }
+
+    async fn create_a_channel() -> (NetworkNode, ChannelInfo, Privkey, Privkey, Privkey) {
+        init_tracing();
+
+        let mut node1 = new_synced_node("node1").await;
+        let capacity = 42;
+        let priv_key: Privkey = get_test_priv_key();
+        let pubkey = priv_key.x_only_pub_key().serialize();
+        let pubkey_hash = &blake2b_256(pubkey.as_slice())[0..20];
+        let tx = TransactionView::new_advanced_builder()
+            .output(
+                CellOutput::new_builder()
+                    .capacity(capacity.pack())
+                    .lock(ScriptBuilder::default().args(pubkey_hash.pack()).build())
+                    .build(),
+            )
+            .output_data(vec![0u8; 8].pack())
+            .build();
+        let outpoint = tx.output_pts()[0].clone();
+        let x_only_pub_key = priv_key.x_only_pub_key();
+        let sk1 = Privkey::from([1u8; 32]);
+        let pk1 = sk1.pubkey();
+        let sk2 = Privkey::from([2u8; 32]);
+        let pk2 = sk2.pubkey();
+
+        let mut channel_announcement = ChannelAnnouncement::new_unsigned(
+            &pk1,
+            &pk2,
+            outpoint,
+            get_chain_hash(),
+            &x_only_pub_key,
+            capacity as u128,
+            None,
+        );
+        let message = channel_announcement.message_to_sign();
+
+        channel_announcement.ckb_signature = Some(priv_key.sign_schnorr(message));
+        channel_announcement.node1_signature = Some(sk1.sign(message));
+        channel_announcement.node2_signature = Some(sk2.sign(message));
+        node1
+            .network_actor
+            .send_message(NetworkActorMessage::Event(NetworkActorEvent::PeerMessage(
+                get_test_peer_id(),
+                FiberMessage::BroadcastMessage(FiberBroadcastMessage::ChannelAnnouncement(
+                    channel_announcement.clone(),
+                )),
+            )))
+            .expect("send message to network actor");
+
+        assert_eq!(node1.submit_tx(tx.clone()).await, Status::Committed);
+
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        let channels = node1.store.get_channels(None);
+        assert_eq!(channels.len(), 1);
+        let channel_info = channels.into_iter().next().unwrap();
+        assert_eq!(&channel_info.announcement_msg, &channel_announcement);
+
+        (node1, channel_info, priv_key, sk1, sk2)
+    }
+
+    #[tokio::test]
+    async fn test_channel_update_version() {
+        let (node, channel_info, _priv_key, sk1, _sk2) = create_a_channel().await;
+
+        let create_channel_update = |version: u64, key: Privkey| {
+            let mut channel_update = ChannelUpdate::new_unsigned(
+                get_chain_hash(),
+                channel_info.announcement_msg.channel_outpoint.clone(),
+                version,
+                0,
+                0,
+                42,
+                0,
+                0,
+                10,
+            );
+
+            channel_update.signature = Some(key.sign(channel_update.message_to_sign()));
+            node.network_actor
+                .send_message(NetworkActorMessage::Event(NetworkActorEvent::PeerMessage(
+                    get_test_peer_id(),
+                    FiberMessage::BroadcastMessage(FiberBroadcastMessage::ChannelUpdate(
+                        channel_update.clone(),
+                    )),
+                )))
+                .expect("send message to network actor");
+            channel_update
+        };
+
+        let channel_update_2 = create_channel_update(2, sk1);
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        let new_channel_info = node
+            .store
+            .get_channels(Some(channel_info.announcement_msg.channel_outpoint.clone()));
+        assert_eq!(new_channel_info.len(), 1);
+        assert_eq!(
+            new_channel_info[0]
+                .node2_to_node1
+                .as_ref()
+                .unwrap()
+                .last_update_message,
+            channel_update_2
+        );
+
+        // Old channel update will not replace the new one.
+        let _channel_update_1 = create_channel_update(1, sk1);
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        let new_channel_info = node
+            .store
+            .get_channels(Some(channel_info.announcement_msg.channel_outpoint.clone()));
+        assert_eq!(new_channel_info.len(), 1);
+        assert_eq!(
+            new_channel_info[0]
+                .node2_to_node1
+                .as_ref()
+                .unwrap()
+                .last_update_message,
+            channel_update_2
+        );
+
+        // New channel update will replace the old one.
+        let channel_update_3 = create_channel_update(3, sk1);
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        let new_channel_info = node
+            .store
+            .get_channels(Some(channel_info.announcement_msg.channel_outpoint.clone()));
+        assert_eq!(new_channel_info.len(), 1);
+        assert_eq!(
+            new_channel_info[0]
+                .node2_to_node1
+                .as_ref()
+                .unwrap()
+                .last_update_message,
+            channel_update_3
+        );
     }
 
     #[tokio::test]
