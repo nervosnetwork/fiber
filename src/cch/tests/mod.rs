1ef77fab67c8bd9bacc987ae3a56e5300f29473c
YI <uuuuuu@protonmail.com>
2025-02-10 17:12:00 +0800

    Create channel between two lnd nodes

--- src/cch/tests/mod.rs
+++ src/cch/tests/mod.rs
@@ -1,7 +1,11 @@
+use std::str::FromStr;
 use std::sync::Arc;
 
+use lnd::bitcoind::bitcoincore_rpc::bitcoin::address::NetworkChecked;
+use lnd::bitcoind::bitcoincore_rpc::bitcoin::Address;
 use lnd::bitcoind::bitcoincore_rpc::RpcApi;
 use lnd::bitcoind::{self, BitcoinD};
+use lnd::tonic_lnd::lnrpc::{GetInfoRequest, GetInfoResponse};
 use lnd::{self, Lnd, LndConf};
 
 fn get_bitcoind_exe_path() -> String {
@@ -31,6 +35,7 @@ impl Default for LndBitcoinDConf {
 pub struct LndNode {
     pub lnd: Lnd,
     pub bitcoind: Arc<BitcoinD>,
+    pub address: Address<NetworkChecked>,
 }
 
 impl LndNode {
@@ -53,7 +58,7 @@ impl LndNode {
 
         let lnd_exe = get_lnd_exe_path();
         let lnd_conf = lnd_conf.unwrap_or_default();
-        let lnd = Lnd::with_conf(
+        let mut lnd = Lnd::with_conf(
             &lnd_exe,
             &lnd_conf,
             cookie.to_string(),
@@ -63,34 +68,123 @@ impl LndNode {
         .await
         .unwrap();
 
-        Self { lnd, bitcoind }
+        use lnd::tonic_lnd::lnrpc::{AddressType, NewAddressRequest};
+        let mut request = NewAddressRequest::default();
+        request.set_type(AddressType::TaprootPubkey);
+        let client = lnd.client.lightning();
+        let address = client
+            .new_address(request)
+            .await
+            .unwrap()
+            .into_inner()
+            .address;
+
+        let address = Address::from_str(&address)
+            .expect("valid address")
+            .assume_checked();
+
+        Self {
+            lnd,
+            bitcoind,
+            address,
+        }
     }
 
     async fn new_lnd_with_the_same_bitcoind(&self, lnd_conf: Option<LndConf<'static>>) -> Self {
         Self::new(lnd_conf, LndBitcoinDConf::Existing(self.bitcoind.clone())).await
     }
+
+    async fn get_info(&mut self) -> GetInfoResponse {
+        self.lnd
+            .client
+            .lightning()
+            .get_info(GetInfoRequest::default())
+            .await
+            .expect("get node info")
+            .into_inner()
+    }
+
+    fn make_some_money(&self) {
+        self.bitcoind
+            .client
+            .generate_to_address(100, &self.address)
+            .expect("Blocks generated to address.");
+    }
+
+    async fn connect(&mut self, other: &mut Self) {
+        let other_info = other.get_info().await;
+        let mut request = lnd::tonic_lnd::lnrpc::ConnectPeerRequest::default();
+        let mut address = lnd::tonic_lnd::lnrpc::LightningAddress::default();
+        address.pubkey = other_info.identity_pubkey;
+        address.host = other
+            .lnd
+            .listen_url
+            .clone()
+            .expect("have listening address");
+        request.addr = Some(address);
+        request.perm = false;
+
+        let response = self
+            .lnd
+            .client
+            .lightning()
+            .connect_peer(request)
+            .await
+            .expect("open channel")
+            .into_inner();
+        println!("connect response: {:?}", response);
+    }
+
+    async fn open_channel_with(&mut self, other: &mut Self) {
+        self.connect(other).await;
+
+        let other_info = other.get_info().await;
+        let mut request = lnd::tonic_lnd::lnrpc::OpenChannelRequest::default();
+        request.node_pubkey_string = other_info.identity_pubkey;
+        request.local_funding_amount = 1_000_000;
+        request.sat_per_vbyte = 1;
+        request.min_confs = 0;
+
+        for _ in 0..10 {
+            match self
+                .lnd
+                .client
+                .lightning()
+                .open_channel_sync(request.clone())
+                .await
+            {
+                Ok(response) => {
+                    println!("open_channel response: {:?}", response);
+                    return;
+                }
+                Err(e) => {
+                    // self.make_some_money();
+                    self.bitcoind
+                        .client
+                        .generate_to_address(100, &self.address)
+                        .expect("Blocks generated to address.");
+                    println!("open_channel error: {:?}", e);
+                }
+            }
+        }
+        panic!("open_channel failed");
+    }
 }
 
 #[tokio::test]
 async fn test_run_lnd_one_node() {
-    use tonic_lnd::lnrpc::GetInfoRequest;
-
     let mut lnd = LndNode::new(Default::default(), Default::default()).await;
     let bitcoin_info = lnd.bitcoind.client.get_network_info();
     assert!(bitcoin_info.is_ok());
-    let node_info = lnd.lnd.client.lightning().get_info(GetInfoRequest {}).await;
-    assert!(node_info.is_ok());
+    println!("node_info: {:?}", lnd.get_info().await);
 }
 
 #[tokio::test]
 async fn test_run_lnd_two_nodes_with_the_same_bitcoind() {
-    use tonic_lnd::lnrpc::GetInfoRequest;
-
     let mut lnd = LndNode::new(Default::default(), Default::default()).await;
     let bitcoin_info = lnd.bitcoind.client.get_network_info();
     assert!(bitcoin_info.is_ok());
-    let node_info = lnd.lnd.client.lightning().get_info(GetInfoRequest {}).await;
-    assert!(node_info.is_ok());
+    println!("node_info: {:?}", lnd.get_info().await);
 
     let mut lnd2 = lnd.new_lnd_with_the_same_bitcoind(Default::default()).await;
     // The second node should be able to run independently of the first node.
@@ -105,4 +199,22 @@ async fn test_run_lnd_two_nodes_with_the_same_bitcoind() {
         .get_info(GetInfoRequest {})
         .await;
     assert!(node_info.is_ok());
+}
+
+#[tokio::test]
+async fn test_run_lnd_two_nodes_with_established_channel() {
+    let mut lnd = LndNode::new(Default::default(), Default::default()).await;
+    let bitcoin_info = lnd.bitcoind.client.get_network_info();
+    assert!(bitcoin_info.is_ok());
+    println!("node_info: {:?}", lnd.get_info().await);
+    lnd.make_some_money();
+
+    let mut lnd2 = lnd.new_lnd_with_the_same_bitcoind(Default::default()).await;
+    let bitcoin_info = lnd2.bitcoind.client.get_network_info();
+    assert!(bitcoin_info.is_ok());
+    println!("node_info: {:?}", lnd.get_info().await);
+    lnd.make_some_money();
+    lnd2.make_some_money();
+
+    lnd.open_channel_with(&mut lnd2).await;
 }
