cd8ff087f4443ade16376c8552de40425c754643
yukang <moorekang@gmail.com>
2025-03-06 18:53:46 +0800

    add rpc for abandon_channel

--- src/rpc/channel.rs
+++ src/rpc/channel.rs
@@ -96,6 +96,12 @@ pub struct OpenChannelResult {
     pub temporary_channel_id: Hash256,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AbandonChannelParams {
+    /// The temporary channel ID or real channel ID of the channel being abandoned
+    pub channel_id: Hash256,
+}
+
 #[serde_as]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AcceptChannelParams {
@@ -316,6 +322,11 @@ trait ChannelRpc {
         params: AcceptChannelParams,
     ) -> Result<AcceptChannelResult, ErrorObjectOwned>;
 
+    /// Abandon a channel, this will remove the channel from the channel manager and DB.
+    /// Only channels not in Ready or Closed state can be abandoned.
+    #[method(name = "abandon_channel")]
+    async fn abandon_channel(&self, params: AbandonChannelParams) -> Result<(), ErrorObjectOwned>;
+
     /// Lists all channels.
     #[method(name = "list_channels")]
     async fn list_channels(
@@ -406,6 +417,16 @@ where
         handle_actor_call!(self.actor, message, params).map(|response| AcceptChannelResult {
             channel_id: response.new_channel_id,
         })
+    }
+
+    async fn abandon_channel(&self, params: AbandonChannelParams) -> Result<(), ErrorObjectOwned> {
+        let message = |rpc_reply| {
+            NetworkActorMessage::Command(NetworkActorCommand::AbandonChannel(
+                params.channel_id,
+                rpc_reply,
+            ))
+        };
+        handle_actor_call!(self.actor, message, params)
     }
 
     async fn list_channels(
