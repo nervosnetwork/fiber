04f15c23369a38b5297e7366783a1642dd9ca4d3
ian <ian@cryptape.com>
2026-02-28 12:25:04 +0800

    feat: implement watchtower querier trait and integrate with network actor
    Allow network/channel actor to query tlc status from watchtower no matter it is in the same process or runs as a standalone service.

--- crates/fiber-lib/src/fiber/network.rs
+++ crates/fiber-lib/src/fiber/network.rs
@@ -653,6 +653,7 @@ pub struct NetworkActor<S, C> {
     store: S,
     network_graph: Arc<RwLock<NetworkGraph<S>>>,
     chain_client: C,
+    watchtower_querier: Option<Arc<dyn super::WatchtowerQuerier>>,
 }
 
 impl<S, C> NetworkActor<S, C>
@@ -676,6 +677,7 @@ where
         store: S,
         network_graph: Arc<RwLock<NetworkGraph<S>>>,
         chain_client: C,
+        watchtower_querier: Option<Arc<dyn super::WatchtowerQuerier>>,
     ) -> Self {
         Self {
             event_sender,
@@ -683,6 +685,7 @@ where
             store: store.clone(),
             network_graph,
             chain_client,
+            watchtower_querier,
         }
     }
 
@@ -1362,8 +1365,19 @@ where
                                     }
                                 }
 
-                                let Some(payment_preimage) = self.store.get_preimage(&payment_hash)
-                                else {
+                                let mut payment_preimage = self.store.get_preimage(&payment_hash);
+                                if payment_preimage.is_none() {
+                                    if let Some(querier) = self.watchtower_querier.as_ref() {
+                                        payment_preimage = querier
+                                            .query_tlc_status(&channel_id, &payment_hash)
+                                            .await
+                                            .and_then(|s| s.preimage);
+                                        if let Some(preimage) = payment_preimage {
+                                            self.store.insert_preimage(payment_hash, preimage);
+                                        }
+                                    }
+                                }
+                                let Some(payment_preimage) = payment_preimage else {
                                     continue;
                                 };
                                 debug!(
@@ -1537,7 +1551,16 @@ where
                                 shared_secret,
                             ) in expired_tlcs
                             {
-                                if self.store.is_tlc_settled(&channel_id, &payment_hash) {
+                                let is_settled =
+                                    if let Some(querier) = self.watchtower_querier.as_ref() {
+                                        querier
+                                            .query_tlc_status(&channel_id, &payment_hash)
+                                            .await
+                                            .is_some_and(|s| s.is_settled)
+                                    } else {
+                                        false
+                                    };
+                                if is_settled {
                                     let (send, _recv) = oneshot::channel();
                                     let rpc_reply = RpcReplyPort::from(send);
                                     if let Err(err) = state
@@ -4971,6 +4994,7 @@ pub async fn start_network<
     store: S,
     network_graph: Arc<RwLock<NetworkGraph<S>>>,
     default_shutdown_script: Script,
+    watchtower_querier: Option<Arc<dyn super::WatchtowerQuerier>>,
 ) -> ActorRef<NetworkActorMessage> {
     let my_pubkey = config.public_key();
 
@@ -4982,6 +5006,7 @@ pub async fn start_network<
             store,
             network_graph,
             chain_client,
+            watchtower_querier,
         ),
         NetworkActorStartArguments {
             config,
---
+++ crates/fiber-lib/src/fiber/watchtower_query.rs
@@ -0,0 +1,23 @@
+use crate::fiber::types::Hash256;
+
+/// Status of a TLC as reported by the watchtower
+pub struct TlcWatchtowerStatus {
+    /// The preimage for the payment hash, if known by the watchtower
+    pub preimage: Option<Hash256>,
+    /// Whether the TLC has been settled on chain
+    pub is_settled: bool,
+}
+
+/// Trait for querying watchtower status of TLCs.
+/// Implemented by direct store access (built-in watchtower) and RPC client (standalone watchtower).
+#[cfg_attr(target_arch = "wasm32", async_trait::async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait::async_trait)]
+pub trait WatchtowerQuerier: Send + Sync {
+    /// Query the watchtower for the status of a TLC identified by channel_id and payment_hash.
+    /// Returns `None` if the watchtower is unreachable or encounters an error.
+    async fn query_tlc_status(
+        &self,
+        channel_id: &Hash256,
+        payment_hash: &Hash256,
+    ) -> Option<TlcWatchtowerStatus>;
+}
