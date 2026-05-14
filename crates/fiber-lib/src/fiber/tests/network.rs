8d57fde494beac48e8ee2b0c72cf4f0d3c6a86cd
yukang <moorekang@gmail.com>
2026-05-14 14:37:09 +0800

    Fix unstable test

--- crates/fiber-lib/src/fiber/tests/network.rs
+++ crates/fiber-lib/src/fiber/tests/network.rs
@@ -432,7 +432,15 @@ async fn test_sync_historical_channel_announcement_on_startup_with_auto_announce
 async fn test_sync_historical_channel_announcement_on_startup_with_auto_announce_disabled() {
     init_tracing();
 
-    let mut node1 = NetworkNode::new_with_node_name("node1").await;
+    let mut node1 = NetworkNode::new_with_config(
+        NetworkNodeConfigBuilder::new()
+            .node_name(Some("node1".to_string()))
+            .fiber_config_updater(|config| {
+                config.auto_announce_node = Some(false);
+            })
+            .build(),
+    )
+    .await;
     let mut node2 = NetworkNode::new_with_config(
         NetworkNodeConfigBuilder::new()
             .node_name(Some("node2".to_string()))
@@ -479,14 +487,16 @@ async fn test_sync_historical_channel_announcement_on_startup_with_auto_announce
         );
     }
 
-    node1.connect_to(&mut node2).await;
     assert!(matches!(
         node2.submit_tx(tx.clone()).await,
         TxStatus::Committed(..)
     ));
+    node1.connect_to(&mut node2).await;
 
-    wait_until_async_timeout(|| async { !node2.get_network_graph_channels().await.is_empty() })
-        .await;
+    wait_until_async_timeout(|| async {
+        node2.get_network_graph_channel(&outpoint).await.is_some()
+    })
+    .await;
 
     let channels = node2.get_network_graph_channels().await;
     assert_eq!(channels.len(), 1);
