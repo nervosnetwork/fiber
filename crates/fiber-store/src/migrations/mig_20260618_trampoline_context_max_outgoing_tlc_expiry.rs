3a9fb679705a3bca741f88d11debb92d1548fbd4
quake <quake.wang@gmail.com>
2026-06-18 23:50:06 +0900

    Update openrpc-json-generator Cargo.lock for fiber-types 0.9.0-rc4

--- crates/fiber-store/src/migrations/mig_20260618_trampoline_context_max_outgoing_tlc_expiry.rs
+++ crates/fiber-store/src/migrations/mig_20260618_trampoline_context_max_outgoing_tlc_expiry.rs
@@ -175,6 +175,18 @@ mod tests {
         crate::Store::open_db(&path).unwrap()
     }
 
+    fn deterministic_pubkey() -> fiber_types_081::Pubkey {
+        #[rustfmt::skip]
+        let bytes: [u8; 33] = [
+            0x02,
+            0x79, 0xbe, 0x66, 0x7e, 0xf9, 0xdc, 0xbb, 0xac,
+            0x55, 0xa0, 0x62, 0x95, 0xce, 0x87, 0x0b, 0x07,
+            0x02, 0x9b, 0xfc, 0xdb, 0x2d, 0xce, 0x28, 0xd9,
+            0x59, 0xf2, 0x81, 0x5b, 0x16, 0xf8, 0x17, 0x98,
+        ];
+        fiber_types_081::Pubkey::from_slice(&bytes).unwrap()
+    }
+
     fn make_old_payment_session(with_trampoline: bool) -> OldPaymentSession {
         let custom_records = PaymentCustomRecords::samples(42)
             .into_iter()
@@ -183,7 +195,7 @@ mod tests {
 
         OldPaymentSession {
             request: SendPaymentData {
-                target_pubkey: fiber_types_081::Pubkey::default(),
+                target_pubkey: deterministic_pubkey(),
                 amount: 1000,
                 payment_hash: fiber_types_081::Hash256::default(),
                 invoice: None,
